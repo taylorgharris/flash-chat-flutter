165bd499bd96a4d24bbd81b65791e038c2ecbe07
Taylor Harris <67606659+taylorgharris@users.noreply.github.com>
2020-11-02 17:26:24 -0800

    on custom animations

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 
 class ChatScreen extends StatefulWidget {
+  static String id = 'chatScreen';
+
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
